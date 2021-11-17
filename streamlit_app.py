6c92f8a38d556b50eec3e8daf2a67cf56cb2ce33
edneide <edneide.ramalho@gmail.com>
2021-11-17 15:27:06 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import folium
+from streamlit_folium import folium_static
+from folium.plugins import MarkerCluster
 
 st.set_page_config(layout='wide')
 
@@ -12,9 +15,18 @@ def get_data(path):
 
     return data
 
+@st.cache( allow_output_mutation=True )
+def get_geofile( url ):
+ geofile = geopandas.read_file( url )
+ return geofile
+
 # Get data
 path = 'datasets/kc_house_data.csv'
 data = get_data(path)
+
+# Get geofile
+url = 'https://opendata.arcgis.com/datasets/83fc2e72903343aabff6de8cb445b81c_2.geojson'
+geofile = get_geofile( url )
 
 # add new feature
 data['price_m2'] = data['price']/data['sqft_lot']
@@ -54,7 +66,6 @@ df = pd.merge(m2, df4, on='zipcode', how='inner')
 df.columns = ['zipcode', 'total houses', 'price', 'sqrt living',
               'price/m2']
 
-st.dataframe(df, height=600)
 
 num_attributes = data.select_dtypes(include=['int64', 'float64'])
 media = pd.DataFrame(num_attributes.apply(np.mean))
@@ -67,12 +78,35 @@ min_ = pd.DataFrame(num_attributes.apply(np.min))
 df1 = pd.concat([max_, min_, media, mediana, std], axis=1).reset_index()
 df1.columns = ['attributes', 'max', 'min', 'mean', 'median', 'std']
 
+st.dataframe(data)
 
-st.dataframe(df1, height=600)
+col1, col2 = st.columns(2)
 
+col1.header('Average Values')
+with col1:
+    st.dataframe(df, height=600, width=800)
 
+col2.header('Descriptive Analysis')
+with col2:
+    st.dataframe(df1, height=600, width=800)
 
+# ============================
+# Densidade de Portfolio
+# ============================
+st.title('Region Overview')
 
+c1, c2 = st.columns((1, 1))
+c1.header('Portfolio Density')
+
+df = data.sample(10)
+
+# Base Map- Folium
+density_map = folium.Map(location=[df['lat'].mean(),
+                                   df['long'].mean()],
+                         default_zoom_start=15)
+
+with c1:
+    folium_static(density_map)
 
 
 
